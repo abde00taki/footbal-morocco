adb4339ba52ec8307c07f96c84af9610ed20a0ee
abde00taki <rramaneabde@gmail.com>
2025-03-12 11:42:31 +0100

    test frant end

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 function validateForms() {
     let errorAlertNom = document.getElementById('error_nom');
-    let errorAlertPrenom = document.getElementById('error_prenom');
-    let errorAlertDate = document.getElementById('error_date');
-    let errorAlertertrendivo=document.getElementById('error_rendivo');
+    let errorAlertPrenom = document.getElementById('error_prenom');   
     let errorAlertertPassword=document.getElementById('error_password');
-    let errorAlertertbillets=document.getElementById('error_billets');
-    let errorAlertertcompts=document.getElementById('error_compts');
+    let errorAlertertemail=document.getElementById('error_email');
+   
 
     let nom = document.forms['validationforms']['nom'].value;
     if (nom === "") {
@@ -24,35 +22,25 @@ function validateForms() {
     } else {
         errorAlertPrenom.classList.add('d-none');
     }
-    let date = document.forms['validationforms']['date'].value;
-    if (date === "") {
-        errorAlertDate.innerHTML = 'Entrez le date';
-        errorAlertDate.classList.remove('d-none');
-        return false;
-    } else {
-        errorAlertDate.classList.add('d-none');
-    }
-    let date_rendivo = document.forms['validationforms']['rendivo'].value;
-    if (date_rendivo === "") {
-        errorAlertertrendivo.innerHTML = 'Entrez le date';
-        errorAlertertrendivo.classList.remove('d-none');
-        return false;
-    } else {
-        errorAlertertrendivo.classList.add('d-none');
-    }
+    
+    
     let password = document.forms['validationforms']['password'].value;
     if (password === "") {
-        errorAlertertPassword.innerHTML = 'Entrez le date';
+        errorAlertertPassword.innerHTML = 'Entrez le motpasse';
         errorAlertertPassword.classList.remove('d-none');
         return false;
     } else {
         errorAlertertPassword.classList.add('d-none');
     }
+    let email = document.forms['validationforms']['email'].value;
+    if (email === "") {
+        errorAlertertemail.innerHTML = 'Entrez email';
+        errorAlertertemail.classList.remove('d-none');
+        return false;
+    } else {
+        errorAlertertemail.classList.add('d-none');
+    }
    
-
-    
-    
-
     // Si tous les champs sont remplis correctement, retourner true
     return true;
 }
